1e1593954c469b50e05fd7141494677b6ad05ee4
John Bryan Valle <jbvalle.jv@gmail.com>
2024-08-22 15:16:59 +0200

    cleaned and finished rcc and gpio drivers for common stm32f4 uC

--- inc/rcc.h
+++ inc/rcc.h
@@ -2,12 +2,6 @@
 #define RCC_H
 #include <stdint.h>
 
-// Function Prototypes
-void RCC_init(void);
-void RCC_EnableClock(uint32_t peripheral);
-void RCC_DisableClock(uint32_t peripheral);
-void RCC_ResetPeripheral(uint32_t peripheral);
-void RCC_SetSysClk(uint32_t clock_source, uint32_t pll_multiplier);
 
 typedef struct{
     volatile uint32_t RCC_CR;
@@ -19,10 +13,24 @@ typedef struct{
     //adapt from here    
 }RCC_TypeDef;
 
+typedef enum{
+    AHB1 = 0x0,
+    AHB2 = 0x1,
+    APB1 = 0x2,
+    APB2 = 0x3
+}RCC_Bridge;
+
 //Adapt this to your Hardware
 #define RCC_BASE_ADDR 0x40023800
 
 //Define pointer to RCC struct
-#define RCC ((RCC_TypeDef*) RCC_BASE_ADDR)
+#define RCC ((RCC_TypeDef *) RCC_BASE_ADDR) 
+
+// Function Prototypes
+void RCC_init(void);
+void RCC_EnableClock(RCC_Bridge bridge, uint32_t peripheral);
+//void RCC_DisableClock(RCC_Bridge bridge, uint32_t peripheral);
+//void RCC_ResetPeripheral(RCC_Bridge bridge, uint32_t peripheral);
+//void RCC_SetSysClk(uint32_t clock_source, uint32_t pll_multiplier);
 
 #endif 
