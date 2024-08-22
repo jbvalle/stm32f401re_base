30d8759826edbab4a4673997a647f6a7923828a5
John Bryan Valle <jbvalle.jv@gmail.com>
2024-08-22 14:41:19 +0200

    upd

--- src/main.c
+++ src/main.c
@@ -5,13 +5,22 @@
 void GPIO_init(void){
 
     RCC->RCC_AHB1ENR |= 1;
+
+    GPIO_B->GPIOx_MODER &= ~(3 << (2 * 14));
+    GPIO_B->GPIOx_MODER |=  (1 << (2 * 14));
+
+    GPIO_B->GPIOx_ODR |= (1 << 14);
 }
+
 
 int main(void){
 
     GPIO_init();
 
-    GPIO_SetMode(PB7, GPIO_MODE_OUTPUT);
+    GPIO_SetMode(PB14, GPIO_MODE_OUTPUT);
 
-    for(;;){}
+    GPIO_WritePin(PB14, 1);
+
+    while(1){
+    }
 }
