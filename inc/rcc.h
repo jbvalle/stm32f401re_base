3954441bdd6c7d59083077d05418b4232c534ada
John Bryan Valle <jbvalle.jv@gmail.com>
2024-08-22 11:07:46 +0200

    added further common fields to rcc

--- inc/rcc.h
+++ inc/rcc.h
@@ -10,7 +10,13 @@ void RCC_ResetPeripheral(uint32_t peripheral);
 void RCC_SetSysClk(uint32_t clock_source, uint32_t pll_multiplier);
 
 typedef struct{
-    
+    volatile uint32_t RCC_CR;
+    volatile uint32_t RCC_PLLCFGR;
+    volatile uint32_t RCC_CFGR;
+    volatile uint32_t RCC_CIR;
+    volatile uint32_t RCC_AHB1RSTR;
+    volatile uint32_t RCC_AHB2RSTR;
+    //adapt from here    
 }RCC_TypeDef;
 
 //Adapt this to your Hardware
